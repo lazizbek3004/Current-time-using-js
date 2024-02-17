d2c808a5c44c72be2964de8b0e587de819814c94
lazizbek3004 <lazizbekturayev194@gmail.com>
2024-02-17 15:01:04 +0500

    milliseconds added

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 
-function uploadTime(){
+
+function myself(){
     const now = new Date();
     const hours = now.getHours().toString().padStart(2, 0);
     const minutes = now.getMinutes().toString().padStart(2, 0);
     const seconds = now.getSeconds().toString().padStart(2, 0);
-    const theTime = `${hours}:${minutes}:${seconds}`
+    const millisecods = now.getMilliseconds();
+    const yengi = millisecods - (millisecods % 100);
+    const result = Number(String(millisecods).slice(0, -2))
+    const current = `${hours}:${minutes}:${seconds}:${result}`
 
+    document.getElementById("clock").textContent = current;
 
-    document.getElementById("clock").textContent = theTime;
 }
 
-setInterval(uploadTime, 1000)
-
-
-
+setInterval(myself, 1)
 
